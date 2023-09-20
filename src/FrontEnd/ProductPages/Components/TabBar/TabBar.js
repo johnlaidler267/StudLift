c1018e3739ebd8135a1b124c0f9150d618a617f2
John Laidler <jlaidler@umass.edu>
2023-09-20 17:49:44 -0400

    fef

--- src/FrontEnd/ProductPages/Components/TabBar/TabBar.js
+++ src/FrontEnd/ProductPages/Components/TabBar/TabBar.js
@@ -15,19 +15,19 @@ const CardGrid = ({ ProductCards }) => {
 export const TabBar = ({ ProductCards }) => {
     return (
         <Tabs defaultActiveKey="all" id="tab-bar" className='mb-3' justify>
-            <Tab eventKey="all" title="ALL (148)" tabClassName="spaced-tabs" onSelect={(eventKey) => handleTabSelection(eventKey, "price")}>
+            <Tab eventKey="all" title="ALL (148)" tabClassName="spaced-tabs" onSelect={(eventKey) => handleTabSelection(eventKey, "all")}>
                 <CardGrid ProductCards={ProductCards} />
             </Tab>
-            <Tab eventKey="headwear" title="HEADWEAR (20)" tabClassName="spaced-tabs">
+            <Tab eventKey="headwear" title="HEADWEAR (20)" tabClassName="spaced-tabs" onSelect={(eventKey) => handleTabSelection(eventKey, "all")}>
                 <CardGrid ProductCards={ProductCards} />
             </Tab>
-            <Tab eventKey="bags" title="BAGS (15)" tabClassName="spaced-tabs">
+            <Tab eventKey="bags" title="BAGS (15)" tabClassName="spaced-tabs" onSelect={(eventKey) => handleTabSelection(eventKey, "all")}>
                 <CardGrid ProductCards={ProductCards} />
             </Tab>
-            <Tab eventKey="socks" title="SOCKS (90)" tabClassName="spaced-tabs">
+            <Tab eventKey="socks" title="SOCKS (90)" tabClassName="spaced-tabs" onSelect={(eventKey) => handleTabSelection(eventKey, "all")}>
                 <CardGrid ProductCards={ProductCards} />
             </Tab>
-            <Tab eventKey="equipment" title="EQUIPMENT (69)" tabClassName="spaced-tabs">
+            <Tab eventKey="equipment" title="EQUIPMENT (69)" tabClassName="spaced-tabs" onSelect={(eventKey) => handleTabSelection(eventKey, "all")}>
                 <CardGrid ProductCards={ProductCards} />
             </Tab>
             <Tab eventKey="bottles" title="BOTTLES (53)" tabClassName="spaced-tabs">
